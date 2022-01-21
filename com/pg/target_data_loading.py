def3f4e4af7efbe145d3aa24ad98833c6b34a205
jit9674 <jit.banerjee9674@gmail.com>
2022-01-21 17:16:34 +0530

    all

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -89,11 +89,11 @@ if __name__ =='__main__':
 
                 jdbc_url=ut.get_redshift_jdbc_url(app_secret)
 
-                ut.read_from_redshift(spark,jdbc_url,
+                mk_df=ut.read_from_redshift(spark,jdbc_url,
                                       "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp",
                                       "select * from {0}.{1} where ins_dt='2022-01-15'".format(app_conf['datamart_schema'],
-                                                                                               tgt_conf["target_src_table"]))\
-                                        .createOrReplaceTempView(tgt_conf['target_src_table'])
+                mk_df.show(5)                                                                               tgt_conf["target_src_table"]))\
+                mk_df.createOrReplaceTempView(tgt_conf['target_src_table'])
 
 
 
